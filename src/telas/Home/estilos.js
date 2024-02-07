3eb675eaf26a2869507d0c34fc081448de4aeb86
Luís Fernando Galeano <contatoluisfernandogaleano@gmail.com>
2024-02-07 17:12:56 -0400

    CONSEGUI CRIAR A TELA POHA

--- src/telas/Home/estilos.js
+++ src/telas/Home/estilos.js
@@ -1,12 +1,19 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 
-export default StyleSheet.create  ({
+export const styles = StyleSheet.create  ({
+    container: {
+        flex: 1,
+        backgroundColor: '#242323',
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+
     home: {
         margin: 0,
         padding: 0,
-        backgroundColor: '#242323',
-
+       
     },
 
     titulo: {
@@ -14,6 +21,27 @@ export default StyleSheet.create  ({
         fontSize: 20,
         fontFamily: 'InterTitulos',
         textAlign: 'center',
+
+    }, 
+
+    botao_home: {
+        color: '#6FAF46',
+        width: 347,
+        height: 65,
+        borderRadius: 10,
+        backgroundColor: '#6FAF46',
+        fontFamily: 'InterRegular',
+        fontSize: 30,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    texto_botao: {
+        color: '#FFFFFF',
+        fontFamily: 'InterTitulos',
+        fontSize: 20,
+        
+
 
     }
 });
--- src/telas/Home/index.js
+++ src/telas/Home/index.js
@@ -1,14 +1,13 @@
-import { View, Text, SafeAreaView,StatusBar} from "react-native";
-import estilos from '../Home/estilos';
+import React from "react";
+import { View, Text, SafeAreaView,StatusBar, Button, Touchable, TouchableOpacity} from "react-native";
+import {styles }from './estilos';
 
 export default function Home(){
     return (
-    <SafeAreaView>
-        <StatusBar/>
-            <View style = {estilos.home}>
-                 <View style = {estilos.home}> 
-                     <Text style = {estilos.titulo}>BEM-VINDO, JOGADOR!</Text>
-                </View>
-            </View>
-    </SafeAreaView>
+        <View style = {styles.container}>
+            <Text style = {styles.titulo}>BEM-VINDO, JOGADOR!</Text>
+            <TouchableOpacity style = {styles.botao_home}>
+                <Text style = {styles.texto_botao}> FUTEBOL MARCADO!</Text>
+                </TouchableOpacity> 
+        </View>
 )}
