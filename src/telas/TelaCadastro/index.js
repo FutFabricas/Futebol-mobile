369223315372f576176c3996a6f55e3287887d3a
jotafreitas <joaocarlofreitas@hotmail.com>
2024-02-13 22:04:23 -0400

    Local do fut salvando no banco

--- src/telas/TelaCadastro/index.js
+++ src/telas/TelaCadastro/index.js
@@ -1,16 +1,32 @@
 import React, {useEffect, useState} from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { estilosCadastro } from './estilos';
+import { createLocal } from '../../service/local';
 
 
 
 export default function TelaCadastro() {
   const navigation = useNavigation();
-
+  const [local, setLocal] = useState('');
   const aindaNada = () => {
     alert("NADA ACONTECEU AINDA");
   };
+
+                const salvarLocal = async () => {
+                  try {
+                    console.log("Adicionando local")
+                    const newLocal = await createLocal({ local: local }); //aqui sou eu tentando adicionar o local
+                    onPress(newLocal);
+                  } catch (error) {
+                    console.log(error);
+                  }
+                };
+
+                const registerLocal = (local) => {
+                  console.log("==========",local);
+                  setData((ant)=>[...ant,local]);
+                }
 
   return (
     
@@ -25,7 +41,9 @@ export default function TelaCadastro() {
         style={estilosCadastro.input}
         placeholder="Local"
         placeholderTextColor="#888"
+        onChangeText={(text) => setLocal(text)}
       />
+
 
       <TextInput
         style={estilosCadastro.input}
@@ -33,10 +51,15 @@ export default function TelaCadastro() {
         placeholderTextColor="#888"
       />
 
+                <Pressable 
+                style={estilosCadastro.botao}
+                onPress={() => salvarLocal()}
+                >
 
-      <TouchableOpacity style={estilosCadastro.botao} onPress={aindaNada}>
-        <Text style={estilosCadastro.textoBotao}>Agendar FUT!</Text>
-      </TouchableOpacity>
+                <Text style={estilosCadastro.textoBotao}>Agendar FUT!</Text>
+
+                  </Pressable>
+       
       
     </View>
     
