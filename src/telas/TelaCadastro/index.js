fa7be91df65cc4fee0d857de11d75b8ee53f3868
Luís Fernando Galeano <contatoluisfernandogaleano@gmail.com>
2024-02-13 20:40:14 -0400

    Tela Marcar Fut Feita

--- src/telas/TelaCadastro/index.js
+++ src/telas/TelaCadastro/index.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { estilosCadastro } from './estilos';
 
@@ -13,43 +13,56 @@ export default function TelaCadastro() {
   };
 
   return (
-    
+    <ScrollView>
     <View style={estilosCadastro.container}>
-      <TouchableOpacity style={estilosCadastro.botaoVoltar} onPress={() => navigation.goBack()}>
-        <Text style={{ color: '#FFF' }}> voltar</Text>
-      </TouchableOpacity>
+      <View style={estilosCadastro.icone}>
+        <TouchableOpacity style={estilosCadastro.botaoVoltar} onPress={() => navigation.goBack()}>
+          <Image source={require('../../../assets/img_voltarTelaFutMarcado.png')}/>
+        </TouchableOpacity>
+      </View>
+      
 
-      <Text style={estilosCadastro.titulo}>Agende o FUT!</Text>
+      <View style={estilosCadastro.icone}>
+        <Text style={estilosCadastro.titulo}>Agende o FUT!</Text>
+        </View>
 
-      <TextInput
-        style={estilosCadastro.input}
-        placeholder="Local"
-        placeholderTextColor="#888"
-      />
+      <View style={estilosCadastro.viewInputs}>
+        <TextInput
+            style={estilosCadastro.input}
+            placeholder="Local"
+            placeholderTextColor="#888"
+          />
 
-      <TextInput
-        style={estilosCadastro.input}
-        placeholder="Horário"
-        placeholderTextColor="#888"
-      />
+          <TextInput
+            style={estilosCadastro.input}
+            placeholder="Horário"
+            placeholderTextColor="#888"
+          />
 
-      <TextInput
-        style={estilosCadastro.input}
-        placeholder="Preço"
-        placeholderTextColor="#888"
-      />
+          <TextInput
+            style={estilosCadastro.input}
+            placeholder="Preço"
+            placeholderTextColor="#888"
+          />
 
-      <TextInput
-        style={estilosCadastro.input}
-        placeholder="Número de Jogadores"
-        placeholderTextColor="#888"
-      />
-
+          <TextInput
+            style={estilosCadastro.input}
+            placeholder="Número de Jogadores"
+            placeholderTextColor="#888"
+          />
+      </View>
+      <View style={estilosCadastro.fundoObservacoes}>
+        <Text style={estilosCadastro.textoObservacoes}>Observações:</Text>
+      </View>
+   
+      <View style={estilosCadastro.icone}> 
       <TouchableOpacity style={estilosCadastro.botao} onPress={handleAgendarPress}>
         <Text style={estilosCadastro.textoBotao}>Agendar FUT!</Text>
       </TouchableOpacity>
+      </View>
       
-    </View>
-    
-  );
-}
+      
+   </View>
+   </ScrollView>
+
+    )};
