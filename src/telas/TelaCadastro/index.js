36b05d6e76036ef22adeca3bc50e40c2a25cb5c3
jotafreitas <joaocarlofreitas@hotmail.com>
2024-02-14 01:01:38 -0400

    ok
    ok

--- src/telas/TelaCadastro/index.js
+++ src/telas/TelaCadastro/index.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from 'react';
 import { View, Text, TextInput, TouchableOpacity, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { estilosCadastro } from './estilos';
-import { createLocal } from '../../service/local';
 import { createHorario } from '../../service/horario';
 
 
@@ -13,37 +12,19 @@ export default function TelaCadastro() {
   const [horario, setHorario] = useState('');
 
   
-
-                // const salvarLocaleHorario = async () => {
-                //   try {
-                //     console.log("Adicionando local")
-                //     const newHorario = await createHorario ({ horario: horario});
-                //     onPress(newHorario);
-                //     const newLocal = await createLocal({ local: local }); //aqui sou eu tentando adicionar o local
-                //     onPress(newLocal);
-                //   } catch (error) {
-                //     console.log(error);
-                //   }
-                // };
-                //ACIMA FOI A PRIMEIRA TENTATIVA E DEU ERRADO
-
-                const salvarLocaleHorario = () => {
-                  try {
-                    console.log("===================Adicionando local e horário");
-                    navigation.navigate('Tabsegundo');
-                    
-                    // Chama a função para criar horário
-                    const newHorario = createHorario({ horario: horario, local: local });
-                    
-                    // Chama a função para criar local
-                    // const newLocal = createLocal({ local: local });
-                    
-                    // Executa a ação onPress com ambas as informações
-                    //onPress(newHorario);
-                  } catch (error) {
-                    console.log(error);
-                  }
-                };
+  const salvarLocaleHorario = () => {
+    try {
+      console.log("===================Adicionando local e horário");
+      navigation.navigate('Tabsegundo');
+      
+      // Chama a função para criar horário
+      const newHorario = createHorario({ horario: horario, local: local });
+      
+    
+    } catch (error) {
+      console.log(error);
+    }
+  };
             
 
   return (
