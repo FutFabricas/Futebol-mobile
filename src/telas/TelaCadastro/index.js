66325c87a30967bd0d589b1ac5ef44489a107f41
jotafreitas <joaocarlofreitas@hotmail.com>
2024-02-16 14:28:24 -0400

    aaaa

--- src/telas/TelaCadastro/index.js
+++ src/telas/TelaCadastro/index.js
@@ -27,7 +27,7 @@ export default function TelaCadastro() {
 
 
   return (
-    <ScrollView>
+   
 
     <View style={estilosCadastro.container}>
       <View style={estilosCadastro.icone}>
@@ -35,8 +35,7 @@ export default function TelaCadastro() {
           <Image source={require('../../../assets/img_voltarTelaFutMarcado.png')}/>
         </TouchableOpacity>
       </View>
-      
-
+    
       <View style={estilosCadastro.icone}>
         <Text style={estilosCadastro.titulo}>Agende o FUT!</Text>
         </View>
@@ -47,7 +46,6 @@ export default function TelaCadastro() {
      placeholderTextColor="#888"
        onChangeText={(text) => setLocal(text)}
       />
-
 
       <TextInput
          style={estilosCadastro.input}
@@ -60,49 +58,13 @@ export default function TelaCadastro() {
         style={estilosCadastro.botao}
          onPress={() => salvarLocaleHorario()}
          >
-
          <Text style={estilosCadastro.textoBotao}>Agendar FUT!</Text>
-
         </Pressable>
       
-      <View style={estilosCadastro.fundoObservacoes}>
-        <Text style={estilosCadastro.textoObservacoes}>Observações:</Text>
-        <TextInput style={estilosCadastro.textoObservacoes}></TextInput>
-      </View>
+  
    
-      <View style={estilosCadastro.icone}> 
-      <TouchableOpacity style={estilosCadastro.botao} onPress={handleAgendarPress}>
-        <Text style={estilosCadastro.textoBotao}>Agendar FUT!</Text>
-      </TouchableOpacity>
-      </View>
+    
 
      </View>
      
-    </ScrollView> 
   )};
-
-  {/* <View style={estilosCadastro.viewInputs}>
-        <TextInput
-            style={estilosCadastro.input}
-            placeholder="Local"
-            placeholderTextColor="#888"
-          />
-
-          <TextInput
-            style={estilosCadastro.input}
-            placeholder="Horário"
-            placeholderTextColor="#888"
-          />
-
-          <TextInput
-            style={estilosCadastro.input}
-            placeholder="Preço"
-            placeholderTextColor="#888"
-          />
-
-          <TextInput
-            style={estilosCadastro.input}
-            placeholder="Número de Jogadores"
-            placeholderTextColor="#888"
-          />
-      </View> */}
